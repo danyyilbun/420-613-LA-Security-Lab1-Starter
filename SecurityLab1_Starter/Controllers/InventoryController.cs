60678db0f11ae08be266c66690a31b27ed3d19f1
Danyyil Bun <danya.boon@gmail.com>
2020-01-16 16:00:08 -0500

    Lab cconnect

--- SecurityLab1_Starter/Controllers/InventoryController.cs
+++ SecurityLab1_Starter/Controllers/InventoryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,20 +12,61 @@ namespace SecurityLab1_Starter.Controllers
         // GET: Inventory
         public ActionResult Index()
         {
+          
             return View();
         }
-         protected override void OnException(ExceptionContext filterContext)
-         {
-             filterContext.ExceptionHandled = true;
-             //Log the error!!
-             _Logger.Error(filterContext.Exception);
-             //Redirect or return a view, but not both.
-             filterContext.Result = RedirectToAction("Index", "Error");
-             // OR
-             filterContext.Result = new ViewResult
-             {
-             ViewName = "~/Views/Error/Index.cshtml"
-             };
-         }
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+
+
+            //Log the error!!
+            if (System.IO.File.Exists(@"log.txt"))
+            {
+                using (StreamWriter w = System.IO.File.AppendText(@"../../log.txt"))
+                {
+                    Log(filterContext.Exception.ToString(), w);
+                }
+
+    
+            }
+            else
+            {
+                using (FileStream fs = System.IO.File.Create(@"../../log.txt"))
+                {
+
+                    using (StreamReader r = System.IO.File.OpenText(@"../../log.txt"))
+                    {
+                        DumpLog(r);
+                        //Redirect or return a view, but not both.
+                        filterContext.Result = RedirectToAction("Index", "Error");
+                        // OR
+                        filterContext.Result = new ViewResult
+                        {
+                            ViewName = "~/Views/Error/Index.cshtml"
+                        };
+                    }
+                }
+            }
+
+        }
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine("  :");
+            w.WriteLine($"  :{logMessage}");
+            w.WriteLine("-------------------------------");
+        }
+
+        public static void DumpLog(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
     }
 }
